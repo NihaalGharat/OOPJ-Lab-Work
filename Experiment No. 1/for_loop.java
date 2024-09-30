ae7f2278c5e8a1eef4241a012a846037201debfe
Nihaal Gharat <nihaalgharat30@gmail.com>
2024-09-30 11:50:31 +0530

    Delete Experiment No. 1 directory

--- Experiment No. 1/for_loop.java
+++
@@ -1,8 +0,0 @@
-public class for_loop{
-    public static void main(String[] args) {
-        // For loop that prints numbers from 1 to 5
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-    }
-}
